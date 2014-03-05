b73a3d326ba413f7990142c50ec408c5d248c2ea
Paul Carver <pcarver@att.com>
2014-03-04 19:28:19 -0500

    Initial working version
    Hooked up and working. Blinks but doesn't fade out.

--- Fireflies/Fireflies.ino
+++ Fireflies/Fireflies.ino
@@ -5,6 +5,7 @@
 const int numberOfFireflies = 8;
 const int firstLED = 3;
 const int maxInterval = 60000; // Longest time between flashes of a specific led
+const int maxDuration = 10000; // Longest time for a firefly to stay lit
 int ledState[numberOfFireflies];
 unsigned long ledStartTime[numberOfFireflies]; // start of flickering on interval
 unsigned long ledTargetDuration[numberOfFireflies]; // duration of on+fadeout
@@ -22,18 +23,22 @@ void setup() {
 void loop() {
   for(int led=0;led< numberOfFireflies;led++){
     if(ledState[led] == LOW){
-      if(millis() - ledStartTime[numberOfFireflies] < ledTargetDuration[numberOfFireflies]){
+      if(millis() < ledStartTime[led]){
+        // Do nothing
+      }
+      else if(millis() - ledStartTime[led] < ledTargetDuration[led]){
         digitalWrite(led + firstLED, HIGH);
         ledState[led] = HIGH;
       }else{
-        ledStartTime[numberOfFireflies] = millis() + random(maxInterval);
+        ledStartTime[led] = millis() + random(maxInterval);
+        ledTargetDuration[led] = random(250, maxDuration);
       }
     }else{
-      if(millis() - ledStartTime[numberOfFireflies] > ledTargetDuration[numberOfFireflies]){
+      if(millis() - ledStartTime[led] > ledTargetDuration[led]){
         digitalWrite(led + firstLED, LOW);
         ledState[led] = LOW;
       }else{
-        unsigned long toggleChance = random(millis() - ledStartTime[numberOfFireflies]);
+        unsigned long toggleChance = random(millis() - ledStartTime[led]);
         // toggleChance is a random number between zero and maxInterval
         // higher numbers are more likely to occur the longer the led has been on
       }
